ae9ff34ef9fe2e3cf3482ded94b90f3339e27b62
Arjun Rajendran <ar5unrajendran@gmail.com>
2023-12-02 20:38:31 +0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
                 className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
-              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in React.js and web development...
               </p>
               <div className="mt-2 flex items-center self-start lg:self-center">
                 <Link
